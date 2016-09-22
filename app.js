b27c6bb8acb9cbbc573639595a7fc4d125e7cf42
Marek <marekfacebook@onet.pl>
2016-09-22 19:29:38 +0200

    This is the final varsion

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var path = require("path");
+var zipdb = require("zippity-do-dah");
+var ForecastIo = require("forecastio");
+
+var app = express();
+var weather = new ForecastIo("5ee290e01573f266ebe9210a113520c1");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+
+ res.render("index");
+});
+
+app.get(/^\/(\d{5})$/, function(req, res, next){
+
+ var zipcode = req.params[0];
+ var location = zipdb.zipcode(zipcode);
+ if(!location.zipcode){
+ next();
+ return;
+}
+ var latitude = location.latitude;
+ var longitude = location.longitude;
+
+ weather.forecast(latitude, longitude, function(err, data){
+
+  if(err){
+  next();
+  return;
+ }
+
+  res.json({
+	zipcode: zipcode,
+	temperature: data.currently.temperature
+
+  });
+ });
+});
+app.listen(4000);
+
